aea1627009261953d7e4848addc55a72752dfb4a
MikeHurley <154265547+MikeHurley@users.noreply.github.com>
2024-01-10 16:45:41 -0500

    creates myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,84 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+  function destructivelyAppendCat(name) {
+    cats.push(name);
+  }
+  destructivelyAppendCat("Ralph");
+
+  beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+  function destructivelyPrependCat(name) {
+    cats.unshift(name);
+  }
+  destructivelyPrependCat("Bob");
+
+  beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+  function destructivelyRemoveLastCat() {
+    cats.pop()
+  }
+
+  beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+  function destructivelyRemoveFirstCat() {
+    cats.shift(0)
+  }
+
+  beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+function appendCat() {
+    return [...cats, "Broom"]
+}
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+function prependCat () {
+    return ["Arnold", ...cats];
+  }
+
+  beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+function removeLastCat() {
+    return cats.slice(0, cats.length -1);
+}
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+  function removeFirstCat () {
+    return cats.slice(1);
+  }
